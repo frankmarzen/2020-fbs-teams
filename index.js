a8fe771c9533026c366adaa21311731052e02c99
Frank Marzen <fmarzen@gmail.com>
2020-08-09 01:41:51 -0400

    make score update change, add logos, etc.

--- index.js
+++ index.js
@@ -11,14 +11,29 @@ request.onreadystatechange = function () {
 function renderTeams(teams) {
     var participatingTeams = 0;
     teams.forEach(team => {
-        var teamElement = document.createElement("li");
-        var teamContent = document.createTextNode(team.name);
-        teamElement.appendChild(teamContent);
+        var teamElement = document.createElement("div");
+        teamElement.classList.add("mb-1", "team");
+
+        if (team.logo) {
+            var teamLogo = document.createElement("img");
+            teamLogo.setAttribute("src", `/data/logos/${team.logo}`);
+            teamLogo.classList.add("team-logo", "mx-auto");
+            var logoContainer = document.createElement("div");
+            logoContainer.appendChild(teamLogo);
+            logoContainer.classList.add("logo-container", "mr-1");
+            teamElement.appendChild(logoContainer);
+        }
+        
+        var teamName = document.createTextNode(team.name);
+        teamElement.appendChild(teamName);
 
         document.getElementById(team.playing ? "participating_teams" : "not_participating_teams").appendChild(teamElement);
         participatingTeams += (team.playing) ? 1 : 0;
     });
 
-    document.getElementById("participating_teams_count").innerHTML = participatingTeams;
+    var participatingTeamsCountElements = document.getElementsByClassName("participating_teams_count");
+    Array.prototype.forEach.call(participatingTeamsCountElements, function(participatingTeam) {
+        participatingTeam.innerHTML = participatingTeams;
+    });
     document.getElementById("not_participating_teams_count").innerHTML = teams.length - participatingTeams;
 }
